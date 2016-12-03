52099844226a0b7686011951d610e458801f434f
pranavdave893 <pranavdave893@gmail.com>
2016-12-02 19:37:49 -0800

    Create FindaString.py

---
+++ FindaString.py
@@ -0,0 +1,24 @@
+"""
+Input Format
+
+The first line of input contains the original string. The next line contains the substring.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input
+
+ABCDCDC
+CDC
+Sample Output
+
+2
+
+"""
+
+str,sub,count=raw_input(),raw_input(),0
+for i in range(len(str)):
+    if str[i:i+len(sub)] == sub:
+        count+=1
+print count

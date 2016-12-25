a4a69fbf9fb7901576340c3e66496d14f0cb60b4
pranavdave893 <pranavdave893@gmail.com>
2016-12-25 15:24:11 -0800

    Create DefaultDict.py

---
+++ Collections/DefaultDict.py
@@ -0,0 +1,13 @@
+/*
+
+Problem statement : https://www.hackerrank.com/challenges/defaultdict-tutorial
+
+*/
+
+from collections import defaultdict
+n,m = map(int,raw_input().split())
+d = defaultdict(list)
+for i in range(n):
+    d[raw_input()].append(i+1)
+for i in range(m):
+    print(' '.join(map(str, d[raw_input()])) or -1)

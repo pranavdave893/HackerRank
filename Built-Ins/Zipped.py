81295d1037301098ca6eef013c6e0b24bb415b4a
pranavdave893 <pranavdave893@gmail.com>
2017-01-09 20:03:23 -0800

    Create Zipped.py

---
+++ Built-Ins/Zipped.py
@@ -0,0 +1,31 @@
+/*
+The format for the general mark sheet is:
+
+Student ID → ___1_____2_____3_____4_____5__               
+Subject 1   |  89    90    78    93    80
+Subject 2   |  90    91    85    88    86  
+Subject 3   |  91    92    83    89    90.5
+            |______________________________
+Average        90    91    82    90    85.5 
+
+Sample Input
+
+5 3
+89 90 78 93 80
+90 91 85 88 86  
+91 92 83 89 90.5
+Sample Output
+
+90.0 
+91.0 
+82.0 
+90.0 
+85.5      
+
+*/
+
+n,x = map(int,input().split())
+mark = [map(float, input().split()) for _ in range(x) ]
+for student in zip(*mark):
+    print(sum(student)/len(student))
+    

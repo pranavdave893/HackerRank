9170b955c918700b90d0d6549524b1752dbf658f
pranavdave893 <pranavdave893@gmail.com>
2017-01-25 15:10:38 -0800

    Create Zeros and Ones.py

---
+++ NumPy/Zeros and Ones.py
@@ -0,0 +1,33 @@
+"""
+Sample Input
+
+3 3 3
+Sample Output
+
+[[[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]]
+[[[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]] 
+  """
+import numpy
+mylist = tuple(map(int,input().split()))
+print (numpy.zeros((mylist), dtype = numpy.int))
+print (numpy.ones((mylist), dtype = numpy.int))
